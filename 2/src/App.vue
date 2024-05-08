2d8e7e656843a6ab8d379d61edd898b8d2d574b3
chaud1024 <chaud1024@gmail.com>
2024-05-08 15:16:43 +0900

    create second folder

---
+++ 2/src/App.vue
@@ -0,0 +1,10 @@
+<script setup>
+import FetchStyle1 from "@/components/FetchStyle1.vue";
+</script>
+
+<template>
+  <h1>App.vue</h1>
+  <FetchStyle1 />
+</template>
+
+<style scoped></style>
---
+++ 2/src/components/FetchStyle1.vue
@@ -0,0 +1,21 @@
+<script setup>
+import { useFetch } from "@/composables/useFetch";
+import { onBeforeMount, ref } from "vue";
+
+const { request } = useFetch();
+
+const data = ref(null);
+
+onBeforeMount(async () => {
+  const { data: result, status, error } = await request("/posts", "get");
+  data.value = result.value;
+});
+</script>
+<template>
+  <h1>FetchStyle1</h1>
+  <div>
+    {{ data }}
+  </div>
+</template>
+
+<style lang="scss" scoped></style>
